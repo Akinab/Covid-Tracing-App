0c4abe05186af3e5351746250caa3d90ad34cae6
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-07-27 14:18:23 +0800

    Details to be filled-out by users that will be saved as a file.

--- covid_tracing_app.py
+++ covid_tracing_app.py
@@ -3,3 +3,9 @@ from tkinter import messagebox
 import os
 
 DATA_FILE = "covid_tracing_data.txt"
+
+def submit_data():
+    name = name_entry.get()
+    location = location_entry.get()
+    date = date_entry.get()
+    contact = contact_entry.get()
